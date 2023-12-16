e11e7ce6374d0a35415d9efe6de817dbc70b9ff6
Nelson Y Jones <153694082+njones3@users.noreply.github.com>
2023-12-16 09:44:17 -0500

    Add files via upload

---
+++ Week2/src/BooleanLab01/BooleanLab1.java
@@ -0,0 +1,27 @@
+package BooleanLab01;
+
+public class BooleanLab1 {
+
+	
+		public static void main(String[] args) {
+
+			  String name = "Tommy";
+
+			  int age = 52;
+
+			  if (name == "Tommy") {
+
+			    System.out.println("Hi, Tommy!");
+
+			  } else if (age == 52) {
+
+			    System.out.println("52 years old.");
+
+			  }
+
+			  System.out.println("Out of conditional.");
+
+			}
+	}
+
+}
